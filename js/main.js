34fc8126a8776d96d4a59809168e5b81f01244c0
jan kopankiewicz <j.kopankiewicz@outlook.com>
2020-07-12 03:31:00 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/categories.html') {
+  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/categories') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
   $('.btn-mobile-menu').click(function () {
